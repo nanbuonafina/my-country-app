56903d5dbf1896763f6f0c23a775866d3d82f60c
Maria Fernanda <124320003+nanbuonafina@users.noreply.github.com>
2024-09-27 10:02:07 -0300

    :lipstick: feat: Estilização CSS CountryDetails

--- frontend/static/js/scripts.js
+++ frontend/static/js/scripts.js
@@ -58,9 +58,9 @@ async function fetchCountryDetails() {
 
     const countryDetails = document.getElementById('countryDetails');
     countryDetails.innerHTML = `
-        <div class="col-md-6">
-            <img src="${country.flags.png}" class="img-fluid mb-4" alt="Flag of ${country.name.common}">
-            <h2>${country.name.common}</h2>
+        <div class="info">
+            <img src="${country.flags.png}" class="info-img img-fluid mb-4" alt="Flag of ${country.name.common}">
+            <h2 class="text-center">${country.name.common}</h2>
             <p><strong>Capital:</strong> ${country.capital}</p>
             <p><strong>Population:</strong> ${country.population}</p>
             <p><strong>Area:</strong> ${country.area} km²</p>
